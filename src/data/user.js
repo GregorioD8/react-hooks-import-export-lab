17d98fe8d37e79dadfa2c3afe95a53347e6050fc
Gregorio <Greg.del801@gmail.com>
2023-08-30 16:56:37 -0700

    completed lab

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,7 @@
 const username = "Liza";
 const city = "New York";
-
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+//With named exports, we can export multiple variables from a file, like so:
+export { username,  city, image }
